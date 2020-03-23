65e4227810ebcf82466583edeacd727528d3bdf8
Alexsandr Vobornov <vobos2@gmail.com>
2020-03-23 16:33:45 -0400

    Added third person camera controller. Added ability to move character around. Jump does not work yet.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float lookSpeed, mouseX, mouseY;
+    public float moveSpeed;
+    public GameObject target;
+    private float dist = 7f;
+
+    private float yAngleMin = 20f;
+    private float yAngleMax = 50f;
+    void Start()
+    {
+        lookSpeed = 4.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX += lookSpeed * Input.GetAxis("Mouse X");
+        mouseY -= lookSpeed * Input.GetAxis("Mouse Y");
+        mouseY = Mathf.Clamp(mouseY, yAngleMin, yAngleMax);
+    }
+    private void LateUpdate()
+    {
+        Vector3 thirdPersonDist = new Vector3(0, 0, -dist);
+        Quaternion rotation = Quaternion.Euler(mouseY, mouseX, 0);
+
+        transform.position = target.transform.position + rotation * thirdPersonDist;
+        transform.LookAt(target.transform.position);
+    }
+
+   
+}
---
+++ Assets/Scripts/characterController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+    public float movementSpeed;
+    private Camera cam;
+    private Rigidbody rb;
+    public bool grounded;
+
+    private void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        moveChar();
+    }
+    void moveChar()
+    {
+
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        input = transform.TransformDirection(input);
+        if (input.magnitude > 1) input.Normalize();
+        float gravity = Physics.gravity.y * 3;
+        Vector3 move = rb.velocity;
+
+        Vector3 p1 = transform.position + Vector3.down * 0.5f;
+
+        if (this.transform.position.y <= .5f)
+        {
+            grounded = true;
+        } else
+        {
+            grounded = false;
+        }
+
+        if (grounded)
+        {
+            move = input * movementSpeed;
+            move.y = gravity * Time.deltaTime;
+
+            if (Input.GetKeyDown("space"))
+            {
+                move.y += 10f ;
+                grounded = false;
+            }
+        }
+        else
+        {
+            move.x = Mathf.Clamp(move.x + input.x * 0.01f * movementSpeed, -movementSpeed, movementSpeed);
+            move.y = Mathf.Clamp(move.y + gravity * Time.deltaTime, gravity, movementSpeed);
+            move.z = Mathf.Clamp(move.z + input.z * 0.01f * movementSpeed, -movementSpeed, movementSpeed);
+        }
+
+        transform.rotation = cam.transform.rotation;
+        Vector3 euler = transform.eulerAngles;
+        euler.x = 0;
+        transform.eulerAngles = euler;
+
+        rb.AddForce(move);
+    }
+}
