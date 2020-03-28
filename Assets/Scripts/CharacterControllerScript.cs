389c4090b5166174a5b433de4cbb20a0aa6f3a17
FeodorD <fedor.dolgov@gmail.com>
2020-03-28 13:49:16 -0400

    WIP added walking FWD and BACK

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+
+  float rotationSpeed = 50;
+  float rotation = 0f;
+
+
+  Animator anim;
+  CharacterController controller;
+
+    void Start()
+    {
+      anim = GetComponent<Animator>();
+      controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+       float moveVertical = Input.GetAxis ("Vertical");
+       float moveHorizontal = Input.GetAxis("Horizontal");
+       print(Input.GetAxis ("Horizontal"));
+
+        anim.SetFloat("vertical", moveVertical);
+        anim.SetFloat("horizontal", moveHorizontal);
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        // if(Input.GetKeyDown(KeyCode.Space) && stateInfo.nameHash == runStateHash)
+        // {
+        //     anim.SetTrigger (jumpHash);
+        // }
+
+
+
+        rotation += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+    //    if(moveHorizontal = 0)
+        transform.eulerAngles = new Vector3(0,rotation,0);
+
+    }
+}
---
+++ Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,17 @@
+// using UnityEngine;
+// using System.Collections;
+//
+// public class CharacterScript : MonoBehaviour {
+//
+// Animator anim;
+//
+//   void Start() {
+//
+//   anim = GetComponent<Animator>();
+//   }
+//
+//   void Update() {
+//     float move = Input.GetAxis("Vertical");
+//     anim.setFloat("Speed");
+//   }
+// }
